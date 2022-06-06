7a1e110067650561d70582aa27fc33a7b31a967a
ytyaru <yttry0@gmail.com>
2022-06-06 14:58:39 +0900

    multipart/form-dataでPOSTした

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,22 +1,25 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     console.log('DOMContentLoaded!!');
-    document.getElementById(`#request`).addEventListener('click', async(event)=>{
+    // https://qiita.com/legokichi/items/801e88462eb5c84af97d
+    document.getElementById(`request`).addEventListener('click', async(event)=>{
         const url = 'https://brid.gy/mastodon/start'
-        const params = {feature: 'listen,publish', instance: 'mstdn.jp', callback: location.href}
+        const params = {feature: 'listen,publish', instance: 'https://mstdn.jp/', callback: location.href}
+        //const body = JSON.stringify(params),
+        const body = Object.keys(params).reduce((o,key)=>(o.set(key, params[key]), o), new FormData());
         const datas = {
             method: 'POST',
-            headers = {
-                'Accept': 'text/html',
-                'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
-            },
-            body: JSON.stringify(params),
+            headers: {'Accept': 'application/json'},
+            body: body,
+//            redirect: 'follow',
         }
-        cosole.log(url)
-        cosole.log(datas)
+        console.log(url)
+        console.log(params)
+        console.log(datas)
         const res = await fetch(url, datas)
         console.log(res)
         const html = await res.text()
         console.log(html)
+        //window.location.href = url
     })
 });
 window.addEventListener('beforeunload', (event) => {
